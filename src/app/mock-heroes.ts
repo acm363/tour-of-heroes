54f0430f7f8ee0762ea13595731d02a05b094f1b
Churchill ATCHEDJI <churchill.atchedji.etu@univ-lille.fr>
2023-07-16 21:07:41 +0200

    feat(heroes): add the heroes data and select hero feature

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 12, name: 'Dr. Nice', description: 'The best hero ever'},
+    {id: 13, name: 'Bombasto', description: 'Bombastic!'},
+    {id: 14, name: 'Celeritas', description: 'Fastest hero ever'},
+    {id: 15, name: 'Magneta', description: 'Magnetic hero'},
+    {id: 16, name: 'RubberMan', description: 'Rubber the incredible hero'},
+    {id: 17, name: 'Dynama', description: 'He can dynamite anything'},
+    {id: 18, name: 'Dr. IQ', description: 'The smartest hero ever'},
+    {id: 19, name: 'Magma', description: 'The hottest hero ever'},
+    {id: 20, name: 'Tornado', description: 'The most destructive hero ever'}
+];
