952bfa21a131a056a9c4c50802b9f682508405b6
Churchill ATCHEDJI <churchill.atchedji@limpidius.com>
2023-08-17 18:29:39 +0200

    feat: add navigation between dashboard, heroes view and hero detail

---
+++ src/app/hero.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {HEROES} from "./mock-heroes";
+import {Hero} from "./hero";
+import {Observable, of} from "rxjs";
+import {MessageService} from "./message.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HeroService {
+
+    constructor(private messageService: MessageService) {
+    }
+
+    getHeroes(): Observable<Hero[]> {
+        const heroes = of(HEROES);
+        this.messageService.add('HeroService: fetched heroes');
+        return heroes;
+    }
+
+    getHero(id: number): Observable<Hero | undefined> {
+        const hero = HEROES.find(hero => hero.id === id);
+        this.messageService.add(`HeroService: fetched hero with id=${id}`);
+        return of(hero);
+    }
+
+}
